e3d72078c10ed8022ebf5cb94b1896fc93bfb424
boneyjohns <boneyjohns007@gmail.com>
2023-04-06 16:12:37 +0530

    new bug

---
+++ android/app/src/main/kotlin/com/edens/Samayam/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.edens.Samayam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- android/app/src/main/kotlin/com/firebase/Samayam/MainActivity.kt
+++ android/app/src/main/kotlin/com/firebase/Samayam/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.samayam
+package com.firebase.Samayam
 
 import io.flutter.embedding.android.FlutterActivity
 
